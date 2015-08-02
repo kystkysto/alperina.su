538919e0336eac493525017b2667e92336b3f506
Efim Solovyev <efim.solovyev@gmail.com>
2015-08-02 21:06:26 +0000

    mobile menu burger

---
+++ public/js/app/body/BodyController.js
@@ -0,0 +1,11 @@
+//public/js/app/body/BodyController.js
+
+angular.module('Alperina.controllers')
+    .controller('BodyController', BodyController);
+
+function BodyController() {
+
+
+    var self = this;
+
+}
