dcba48681b27a6619a223fef653e3d9bad6f5034
kystkysto <efim.solovyev@gmail.com>
2015-10-21 23:14:20 +0300

    cleanup

--- db/migrate/20150921201226_add_confirmable_to_devise_v1.rb
+++
@@ -1,7 +0,0 @@
-class AddConfirmableToDeviseV1 < ActiveRecord::Migration
-  def change
-    change_table(:users) do |t| 
-        t.confirmable 
-    end 
-  end
-end
--- db/migrate/20150921202616_addconfirmable.rb
+++
@@ -1,7 +0,0 @@
-class Addconfirmable < ActiveRecord::Migration
-    def self.up 
-        change_table(:users) do |t|
-            t.boolean :confirmable, :default => false
-        end
-    end 
-end
